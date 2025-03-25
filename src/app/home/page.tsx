34894a7ef2356c4b52d9aea250846ec9a465cec5
Gheve Mombay <gheve.mombay97@gmail.com>
2025-03-26 01:40:34 +0800

    Add a new page after login is successful

---
+++ src/app/home/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+const HomePage = () => {
+  const router = useRouter();
+
+  const GotoLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    router.push("/");
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <label className="block text-lg font-medium mb-2">Home Page</label>
+      </div>
+      <button
+        type="submit"
+        className="mb-4 w-full py-2 bg-indigo-600 text-white rounded-md text-lg hover:bg-indigo-700"
+        onClick={GotoLogin}
+      >
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default HomePage;
